efce97c4658cc5ed698f83b00fe4bfb5996803e4
Sebastian <haggblca@arcada.fi>
2023-10-10 16:30:20 +0300

    made changes to deploy to render

--- chat and board/backend/server.js
+++ chat and board/backend/server.js
@@ -28,6 +28,19 @@ app.use("/api/message", messageRoutes);
 app.use("/api/board", boardRoutes);
 app.use("/api/note", noteRoutes);
 
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
